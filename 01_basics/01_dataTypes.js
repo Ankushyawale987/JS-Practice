856045c9ee7b6d78ae0f44220012eb9aee59998c
Ankush Yawale <97789968+Ankushyawale987@users.noreply.github.com>
2024-08-29 10:19:11 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3) // we are using nodejs, brower
+/* 
+number
+string
+boolean
+undefined
+null
+bigint
+symbol
+*/
+
+console.log(typeof null); // typeof null is object
+console.log(typeof undefined);// typeof undefined is undefined
