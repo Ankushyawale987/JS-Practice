a62dd1199079cbb6f96fdf68e8ffd85230619982
Ankush Yawale <97789968+Ankushyawale987@users.noreply.github.com>
2024-08-31 12:23:22 +0000

    comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2); // it perform strick check value along with data type
+console.log("2" == 2); // it perform type conversion so ans will true
