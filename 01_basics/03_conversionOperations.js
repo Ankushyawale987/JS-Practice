7e444ee23a58b75c4489bbe664391b5fcead6f9c
Ankush Yawale <97789968+Ankushyawale987@users.noreply.github.com>
2024-08-31 11:42:46 +0000

    Conversions of DataTypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,36 @@
+let score = 33;
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let s = "33d";
+//console.log(typeof s);
+let valueInNumber = Number(s);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+let i = null; /// trite as object
+//console.log(typeof i);
+
+let j = undefined;
+//console.log(typeof j);
+
+// "33" => 33 // Number
+// "33abb" => NaN // Not a Number
+// true=>1; false=>0
+
+
+let isLoggedIn = 1;
+// let isLoggedIn = "Ankush"; //true
+// let isLoggedIn = ""; //false
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Ankush" => true
+
+let a = 33;
+let str = String(a);
+console.log(str);
+console.log(typeof str);
