100ee2eafc76e1c57dfba03db12710a183fad814
Ankush Yawale <97789968+Ankushyawale987@users.noreply.github.com>
2024-08-31 11:08:21 +0000

    Const, let, var

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,10 @@
+const accountId = 14356;
+let accountEmail = "ankushyawale02@gmail.com";
+var accountName = "Ankush";
+accountCity = "Amravati";
+let customerAddress;// undefined
+console.table([accountId, accountEmail, accountName, accountCity, customerAddress]);
+
+// const Variable - Can't Update or Redeclare
+// let Variable - Can't Redeclare But can update
+// varVariable - Allow to update and redeclare
