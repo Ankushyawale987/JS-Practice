75b93ee00c6127a3c4e0edc158a2c51ae13a9563
Ankush Yawale <97789968+Ankushyawale987@users.noreply.github.com>
2024-09-03 10:04:57 +0000

    Datatypes-Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,46 @@
+// Primitive
+
+// 7 types of Datatypes: String, Number,Boolean, null, undefined, Symbol, BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)// not same
+
+
+
+const bigNumber = 2345647n;
+
+
+// Reference (Non Primitive)
+ // Array, Object, Function
+
+
+ const heros = ["Shaktiman", "Naagraj", "Doga"]
+
+ let myObj = {
+    name : "ANkush",
+    age : 22,
+    num: 234
+ };
+
+  const myFunction = function() {
+    console.log("Hello World");
+  }
+
+  /**
+   Datatype - typeof
+   Undefined-undefined
+   Null-object
+   Boolean-boolean
+   Number-number
+   String-string
+   
+
+   */
