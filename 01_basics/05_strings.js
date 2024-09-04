afac60eebf45be83d95d9743efa4a1d27d093140
Ankush Yawale <97789968+Ankushyawale987@users.noreply.github.com>
2024-09-04 06:43:57 +0000

    String Basics and Functions

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "Ankush"
+const repoCount = 50;
+
+//console.log(name + repoCount + " Value"); // It is old days Usage Pattern
+
+console.log(`My name is ${name} and my repo count is ${repoCount}`)
+// It is `` not ''
+
