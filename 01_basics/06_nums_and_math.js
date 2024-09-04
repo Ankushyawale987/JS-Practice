6801f87702a9f960e30b96602e18ea4b7efd5850
Ankush Yawale <97789968+Ankushyawale987@users.noreply.github.com>
2024-09-04 08:15:19 +0000

    Number and Maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,23 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+// it will return the 2 number after the 2
+const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toPrecision(2))
+// console.log(otherNumber.toPrecision(1))
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
